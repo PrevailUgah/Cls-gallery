9e42b9b3284d28df3705a347796d653db1cdba6d
Prevail Ugah <prevailbugah@gmail.com>
2023-04-08 14:22:44 +0200

    gallery update

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$(window).on('scroll',function(){
+    var scroll = $(window).scrollTop();
+if(scroll>=50){
+$(".sticky").addClass("stickyadd");
+}else{
+    $(".sticky").removeClass("stickyadd");
+}
+})
+var typed = new Typed(".element",{
+    strings:["Welcome to our gallery..."],
+    smartBackspace:true,
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true,
+    loopCount:Infinity,
+    startDelay:1000
+});
+});
